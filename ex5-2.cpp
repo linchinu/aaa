3ccd32164cef3ac3862fded4f4ce932170775772
linchinu <104434749+linchinu@users.noreply.github.com>
2022-05-24 22:01:23 +0800

    Create ex5-2.cpp

---
+++ ex5-2.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+using namespace std;
+int main()
+{
+   int i,j;
+   
+   for(i=1;i < 10;i++){
+       for(j=1;j < 10;j++){
+          printf("%2d*%2d=%2d \n",i,j,i*j);
+            
+        } 
+       
+   }
+   
+    return 0;
+}
