fcc4662f1d6e038a8e7af5766f6260e99c7bbe5f
linchinu <20351103@nhush.tp.edu.tw>
2022-06-12 13:55:15 +0000

    update

---
+++ ex7-6.cpp
@@ -0,0 +1,19 @@
+
+/***
+ 溫度換算c換f
+***/
+#include <stdio.h>
+int main()
+{
+    int C,F;
+    
+    printf("please input degree(C) \n");
+    printf("C = \n");
+    scanf("%d",&C);
+    
+    F = 9*C/5+32;
+    printf("F = %d \n",F);
+    
+    return 0;
+}
+
